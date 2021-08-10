8b729f17b920520a6a8a243ed5c15a337ba135dd
christinaguliuzza <christina.guliuzza@gmail.com>
2021-08-10 16:10:35 -0700

    commit tree eaf1f31ee3bfbf48eb43241a37b986e17121bad6

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+
+// razzle();
+function razzle() {
+    console.log("You've been razzled!");
+}
+// razzle();
+
+
+// function saturdayFun(activity="roller-skate") {
+//     console.log(`This Saturday, I want to ${activity}!`)
+// }
+
+// saturdayFun();
+
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(commute="go to the office") {
+    return `This Monday, I will ${commute}.`
+}
+
+function wrapAdjective(highlight='*') {
+    return function(pro="a hard worker") {
+        return `You are ${highlight}${pro}${highlight}!` 
+    }
+}
+
